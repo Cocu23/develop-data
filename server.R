874b1e224d6537ef1b4b15a35002b4d7f461992c
Cocu23 <c_kukuk@hotmail.com>
2016-03-05 18:25:21 +0100

    Added files via upload

---
+++ server.R
@@ -0,0 +1,18 @@
+library(shiny)
+
+shinyServer(
+  function(input, output){
+    conversionFactor <- reactive({
+      # Fahrenheit to Celsius Conversion
+      if(as.character(input$unit) == "Degrees Fahrenheit"){
+        paste("=", (input$temperature - 32)*5/9, "Degrees Celsius")
+      }
+      # Celsius to Fahrenheit Conversion
+      else{
+        paste("=", input$temperature*9/5 + 32, "Degrees Fahrenheit")
+      }
+    })
+    
+    output$converted_temp = renderText({paste(conversionFactor())})
+  }
+)
---
+++ ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  headerPanel("Temperature Unit Converter"),
+  sidebarPanel(
+    numericInput("temperature", label = "Enter Temperature Here:", value = 0),
+    
+    radioButtons("unit", label = "Select Original Units Here:", 
+                 choices = c("Degrees Fahrenheit", "Degrees Celsius"))
+  ),
+  mainPanel(
+    h4("Converted Temperature"),
+    
+    verbatimTextOutput("converted_temp"),
+    
+    h4("This app takes temperatures in degrees Celsius and carries
+       out the calculations to get the temperature in degrees
+       Fahrenheit, and vice versa."),
+    
+    h4("Enter the temperature to be converted and select its given units using
+       the inputs to the left."),
+    
+    h4("The app automatically converts to the unselected temperature
+       unit when you enter a temperature."),
+    
+    h4("For example, if degrees Fahrenheit is selected as the entered
+       temperature's unit the app automatically converts to
+       degrees Celsius.")
+    )

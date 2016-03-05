13d7d99205d0459dbb19b0a4ccff5986a25cacdd
Cocu23 <c_kukuk@hotmail.com>
2016-03-05 18:37:23 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,29 +1,23 @@
 library(shiny)
 
 shinyUI(pageWithSidebar(
-  headerPanel("Temperature Unit Converter"),
+  headerPanel("Celsius-Fahrenheit Temperature Converter"),
   sidebarPanel(
     numericInput("temperature", label = "Enter Temperature Here:", value = 0),
     
-    radioButtons("unit", label = "Select Original Units Here:", 
-                 choices = c("Degrees Fahrenheit", "Degrees Celsius"))
+    radioButtons("unit", label = "Select Degree Here:", 
+                 choices = c(" Fahrenheit", " Celsius"))
   ),
   mainPanel(
     h4("Converted Temperature"),
     
     verbatimTextOutput("converted_temp"),
     
-    h4("This app takes temperatures in degrees Celsius and carries
-       out the calculations to get the temperature in degrees
-       Fahrenheit, and vice versa."),
+    h4("Here you can change temperatures in degrees Celsius and/or Fahrenheit
+       "),
     
-    h4("Enter the temperature to be converted and select its given units using
-       the inputs to the left."),
+    h4("Enter the temperature to be converted and select the input degree for converting")
     
-    h4("The app automatically converts to the unselected temperature
-       unit when you enter a temperature."),
-    
-    h4("For example, if degrees Fahrenheit is selected as the entered
-       temperature's unit the app automatically converts to
-       degrees Celsius.")
+   
     )
+))
